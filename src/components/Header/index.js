5d6d8fffdf7a348de6e00c3ea005a17eca6a825c
David Escobar <davidescobarmx@gmail.com>
2023-07-04 18:30:01 -0600

    correcion de error eslint

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import "./Header.css"
 import Boton from "../Button"
+import logo from '../../logo.png'
 
 function Header(props) {
 
     return <header className="header">
-        <img src='../img/logo.png' alt='AluraFlix'/>
+        <img src={logo} alt='AluraFlix'/>
         <Boton titulo="Nuevo video" action="agregarVideo" cambioEstado={props.cambioEstado}/>
     </header>
 }
